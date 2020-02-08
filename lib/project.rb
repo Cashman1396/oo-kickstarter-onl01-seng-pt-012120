e661bcd0bbccdc1839bc848b8acce6978d1192e8
Chris Ashman <chrisashman96@gmail.com>
2020-02-08 19:51:39 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,4 +4,10 @@ class Project
   def initialize(title)
     @title = title 
     @backers = [] 
+  end 
   
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects  << self 
+  end
+end 
